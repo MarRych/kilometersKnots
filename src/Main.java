37edac70d774152fbf9c21794097375f3f32ae8f
marrych <rycmar38@gmail.com>
2022-11-27 17:15:24 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        //13. Napisz program pozwalający na przeliczenie prędkości
+        // zadanej w kilometrach na godzinę na prędkość wyrażoną w węzłach. (poziom 1)
+        double kph, knots;
+
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter how many kilometers/hour: ");
+
+        kph = scanner.nextDouble();
+        knots = (kph * 1.85);
+
+        System.out.printf("Knots:  %.2f" ,knots);
+
+    }
+}
