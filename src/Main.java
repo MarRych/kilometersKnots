e99716a133be437b67b1676572b842785b337cfb
marrych <rycmar38@gmail.com>
2023-04-20 22:20:38 +0200

    first test commit

--- src/Main.java
+++ src/Main.java
@@ -5,16 +5,26 @@ public class Main {
 
         //13. Napisz program pozwalający na przeliczenie prędkości
         // zadanej w kilometrach na godzinę na prędkość wyrażoną w węzłach. (poziom 1)
-        double kph, knots;
-
-
+        /*
+           double kph, knots;
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Enter how many kilometers/hour: ");
+            kph = scanner.nextDouble();
+            knots = (kph * 1.85);
+            System.out.printf("Knots:  %.2f", knots);
+*/
+        double kph;
+        double knots ;
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter how many kilometers/hour: ");
-
         kph = scanner.nextDouble();
-        knots = (kph * 1.85);
 
-        System.out.printf("Knots:  %.2f" ,knots);
+        System.out.print(multiply(kph,1.85));
 
+    }
+
+    private static double multiply(double kph, double knots)  {
+       double result = kph * knots;
+       return result;
     }
 }
---
+++ src/MainTest.java
@@ -0,0 +1,15 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+/*
+    void fiveKilometersTurnForKnots(){
+        Main calculator = new Main();
+        assertEquals(18.50,calculator.main(10,));
+
+    }
+*/
+
+}
